0f94dc4cb199ff64930b40675b7827e0879803e2
iFatma2 <fatma.m.q@outlook.sa>
2024-09-29 09:24:50 +0300

    Initial Commit

---
+++ AppPrac/AppPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppPracApp.swift
+//  AppPrac
+//
+//  Created by Fatimah Alqarni on 26/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct AppPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
